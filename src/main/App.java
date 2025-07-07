92de318a9e2cd23bccd5b4cf62e193293d4d03f2
CMSebas <rafaselm918@gmail.com>
2025-07-06 23:37:29 -0500

    Estructuras No Lineales – Ejercicios Árboles

--- src/main/App.java
+++ src/main/App.java
@@ -2,32 +2,102 @@ package main;
 
 import utils.StudentValidator;
 import main.Materia.Models.Node;
+
+import java.util.LinkedList;
+import java.util.List;
+
 import main.Ejercicio_01_insert.InsertBST;
+import main.Ejercicio_02_invert.InvertBinaryTree;
+import main.Ejercicio_03_listLeves.ListLevels;
+import main.Ejercicio_04_depth.Depth;
 import main.Materia.Controllers.ArbolRecorridos;
 
 
 
 public class App {
     public static void main(String[] args) throws Exception {
-        ArbolRecorridos arbolRecorridos=new ArbolRecorridos();
         // NO MODIFICAR ESTE CÓDIGO
         var estudiante = StudentValidator.validarDatos();
         System.out.println("👤 Estudiante: " + estudiante.nombre());
         System.out.println("✉️  Correo: " + estudiante.correo());
         // NO MODIFICAR HASTA AQUÍ
 
+        //PRIMER EJERCICIO
+        System.out.println("Ejercicio numero 1");
 
-        
-        int[] valoresEj1 = {5, 3, 7, 2, 4, 6, 8};
+        ArbolRecorridos arbolRecorridos=new ArbolRecorridos();
+        int[] numeros = {5, 3, 7, 2, 4, 6, 8};
         InsertBST insertador = new InsertBST();
-        Node raizEj1 = null;
-        for (int v : valoresEj1) {
-            raizEj1 = insertador.insert(raizEj1, v);
+        Node raiz1 = null;
+        for (int num : numeros) {
+            raiz1 = insertador.insert(raiz1, num);
         }
         System.out.println("Input: [5, 3, 7, 2, 4, 6, 8]");
-        System.out.print("(In-Order): ");
-        arbolRecorridos.inOrderTraversal(raizEj1);
+        System.out.println("En In-Order: ");
+        arbolRecorridos.inOrderTraversal(raiz1);
+        System.out.println("");
+
+        //SEGUNDO EJERCICIO
+        ;System.out.println("Ejercicio 2");
+
+        Node root2 = new Node(4);
+        root2.setLeft(new Node(2));
+        root2.setRight(new Node(7));
+        root2.getLeft().setLeft(new Node(1));
+        root2.getLeft().setRight(new Node(3));
+        root2.getRight().setLeft(new Node(6));
+        root2.getRight().setRight(new Node(9));
+        System.out.println("Input ingresado en In-Order: ");
+        arbolRecorridos.inOrderTraversal(root2);
+        InvertBinaryTree inverter = new InvertBinaryTree();
+        Node invertido = inverter.invertTree(root2);
+        System.out.println(" ");
+        System.out.println("Output en In-order ");
+        arbolRecorridos.inOrderTraversal(invertido);
+        System.out.println("");
+        //TERCER EJERCICIO
+        System.out.println("Ejercicio 3");
+        Node root3 = new Node(4);
+        root3.setLeft(new Node(2));
+        root3.setRight(new Node(7));
+        root3.getLeft().setLeft(new Node(1));
+        root3.getLeft().setRight(new Node(3));
+        root3.getRight().setLeft(new Node(6));
+        root3.getRight().setRight(new Node(9));
+
+        ListLevels listar = new ListLevels();
+        List<LinkedList<Node>> niveles = listar.obtenerNiveles(root3);
+
+        
+        int nivel = 0;
+        for (LinkedList<Node> lista : niveles) {
+            System.out.print("Nivel " + nivel + ": ");
+            for (Node n : lista) {
+                System.out.print(n.getValue() + " ");
+            }
+            System.out.println();
+            nivel++;
+        }
+        //CUARTO EJERCICIO
+
+        System.out.println("Ejercicio 4");
+
+        Node root4 = new Node(4);
+        root4.setLeft(new Node(2));
+        root4.setRight(new Node(7));
+        root4.getLeft().setLeft(new Node(1));
+        root4.getLeft().setRight(new Node(3));
+        root4.getLeft().getLeft().setLeft(new Node(8));
+        Depth calProf = new Depth();
+        int profundidad = calProf.maxDepth(root4);
+        System.out.println("Profundidad máxima del árbol: " + profundidad);
+    
         
     }
+
+
+    
+
+    
 
 }
---
+++ src/main/Ejercicio_03_listLeves/ListLevels.java
@@ -0,0 +1,41 @@
+package main.Ejercicio_03_listLeves;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import main.Materia.Models.Node;
+
+public class ListLevels {
+     public List<LinkedList<Node>> obtenerNiveles(Node root) {
+        List<LinkedList<Node>> response = new ArrayList<>();
+
+        if (root == null) {
+            return response;
+        }
+
+        Queue<Node> cola = new LinkedList<>();
+        cola.offer(root);
+
+        while (!cola.isEmpty()) {
+            int tamañoNivel = cola.size();
+            LinkedList<Node> nivel = new LinkedList<>();
+
+            for (int i = 0; i < tamañoNivel; i++) {
+                Node actual = cola.poll();
+                nivel.add(actual);
+
+                if (actual.getLeft() != null) {
+                    cola.offer(actual.getLeft());
+                }
+                if (actual.getRight() != null) {
+                    cola.offer(actual.getRight());
+                }
+            }
+
+            response.add(nivel);
+        }
+
+        return response;
+    }
+}
